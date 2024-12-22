cd0b9d821e88478e72121e73ab4832a5c89d6931
caohao <18500482693@163.com>
2024-12-22 23:14:14 +0800

    modify author and copyright
    modify author and copyright

--- log/log.go
+++ log/log.go
@@ -1,3 +1,16 @@
+// Copyright (c) 2024 The horm-database Authors (such as CaoHao <18500482693@163.com>). All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package log
 
 import (
@@ -12,7 +25,7 @@ import (
 // NewTimeLog 获取超时日志
 func NewTimeLog(ctx context.Context, addr *util.DBAddress) *log.TimeLog {
 	//在请求响应时长超过 warn_timeout 毫秒的时候告警
-	warnTimeout := 200 * time.Millisecond 
+	warnTimeout := 200 * time.Millisecond
 	if addr.WarnTimeout > 0 {
 		warnTimeout = time.Duration(addr.WarnTimeout) * time.Millisecond
 	}
